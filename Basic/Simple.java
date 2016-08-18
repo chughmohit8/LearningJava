147b64a3508722581f369343652e14e9c829b80e
chughmohit8 <chughmohit8@gmail.com>
2016-08-18 19:17:05 +0530

    First Push

---
+++ Basic/Simple.java
@@ -0,0 +1,12 @@
+class Simple {
+
+
+    public static void main(String[] u) {
+
+
+        System.out.println("Khool"); //to print on console
+        System.out.print("Khool"); //to print on console
+
+
+    }
+}
