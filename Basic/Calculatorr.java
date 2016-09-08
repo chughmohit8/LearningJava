18581e590ea0e758671f7157ca57b18b66b92c25
chughmohit8 <chughmohit8@gmail.com>
2016-09-08 07:21:17 +0530

    Calculator

---
+++ Basic/Calculatorr.java
@@ -0,0 +1,22 @@
+class Calculatorr
+{
+public static void main(String[] args)
+{
+char n='+';
+int q=10,r=20;
+switch(n)
+{
+case '+':
+System.out.println("Addition is"+ (q+r));
+break;
+
+case '-':
+System.out.println("Subtraction is"+ (q-r));
+break;
+
+case '*':
+System.out.println("Multiplication is"+ (q*r));
+break;
+}
+}
+}
